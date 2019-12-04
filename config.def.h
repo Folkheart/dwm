d35cd76e35fd08c3e6a3efb37b73a83d1e1292c9
Folkheart <leandroadrianpaz@gmail.com>
2019-12-04 02:00:07 -0300

    xmonad2

--- config.def.h
+++ config.def.h
@@ -94,6 +94,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_k,      attachsup,      {0} },
 };
 
 /* button definitions */
