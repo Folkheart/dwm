7b6f84e8b25ccef6e689017d2a538630ec16dc86
Folkheart <leandroadrianpaz@gmail.com>
2019-12-23 17:29:27 -0300

    uselessgap

--- config.def.h
+++ config.def.h
@@ -1,7 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -18,6 +17,8 @@ static const char *colors[][3]      = {
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
+
+static const unsigned int gappx     = 6;        /* gaps between windows */
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
